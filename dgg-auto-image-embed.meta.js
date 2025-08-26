d95a6d0519c852aab3403567a445c4a3f9747a7f
Julius GD <juliusdorfman@gmail.com>
2025-08-25 21:37:50 -0700

    hosting on github

---
+++ dgg-auto-image-embed.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        DGG Auto Image Embed
+// @namespace   boofus
+// @version     10.0.1
+// @description Shows a embedded preview of images linked in destiny.gg
+// @match       https://www.destiny.gg/embed/chat*
+// @icon        https://cdn.destiny.gg/2.49.0/emotes/6296cf7e8ccd0.png
+// @author      boofus (credits: legolas, vyneer)
+// ==/UserScript==
