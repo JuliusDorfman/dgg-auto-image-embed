028a1d785ab242d29eadfc19617c3f0c2f925897
Julius Dorfman <juliusdorfman@gmail.com>
2026-02-28 12:48:51 -0800

    added kappa.lol and another image hosting service

--- dgg-auto-image-embed.meta.js
+++ dgg-auto-image-embed.meta.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name        DGG Auto Image Embed (10.3.2 + Imgur album support)
+// @name        DGG Auto Image Embed (Actively Maintained + Growing list of Img Hosts)
 // @namespace   boofus
 // @description Preview images linked in destiny.gg chat. NSFW blur, hide-link, sticky autoscroll, optional 4chan block. Stable de-duplication + Imgur page/album resolver.
 // @match       https://www.destiny.gg/embed/chat*
 // @icon        https://cdn.destiny.gg/2.49.0/emotes/6296cf7e8ccd0.png
-// @version     10.3.2
-// @author      boofus (credits: legolas, vyneer)
+// @version     10.5.0
+// @author      boofus (credits: legolas, vyneer, cyclone, Torrniquet)
 // @license     MIT
 // @grant       GM_xmlhttpRequest
 // @run-at      document-end
@@ -21,6 +21,9 @@
 // @connect     files.catbox.moe
 // @connect     catbox.moe
 // @connect     i.4cdn.org
-// @downloadURL https://update.greasyfork.org/scripts/547316/DGG%20Auto%20Image%20Embed.user.js
-// @updateURL   https://update.greasyfork.org/scripts/547316/DGG%20Auto%20Image%20Embed.meta.js
+// @connect     i.kym-cdn.com
+// @connect     femboy.beauty
+// @connect     kappa.lol
+// @downloadURL https://update.greasyfork.org/scripts/547316/DGG%20Auto%20Image%20Embed%20%28Actively%20Maintained%20%2B%20Growing%20list%20of%20Img%20Hosts%29.user.js
+// @updateURL https://update.greasyfork.org/scripts/547316/DGG%20Auto%20Image%20Embed%20%28Actively%20Maintained%20%2B%20Growing%20list%20of%20Img%20Hosts%29.meta.js
 // ==/UserScript==
